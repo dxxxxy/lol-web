745a88d5f60a3ebd0ca73a0e1b1507bca5a597e9
DxxxxY <introsdream@gmail.com>
2021-10-10 19:14:24 -0400

    borders, lobby fix, cleanup

--- preload.js
+++ preload.js
@@ -4,7 +4,6 @@ const LCUConnector = require('lcu-connector')
 const connector = new LCUConnector()
 const fetch = require("node-fetch")
 
-
 let auth = {}
 
 const get = (endpoint) => {
@@ -68,15 +67,6 @@ const put = (endpoint, body) => {
     })
 }
 
-
-//replace everything with 
-
-// get("/lol-lobby/v2/lobby/members").then(res => {
-//     Object.keys(res).forEach(player => {
-//         console.log(res[player])
-//     })
-// })
-
 /*
 {
   allowedChangeActivity: true,
@@ -106,23 +96,25 @@ const put = (endpoint, body) => {
   summonerName: 'yurms',
   teamId: 0
 }
-
 */
+
 const createPlayer = (name, icon, id, lvl, percent) => {
     var fieldset = document.createElement("fieldset")
     var legend = document.createElement("legend")
     var img = document.createElement("img")
+    var border = document.createElement("img")
     var p = document.createElement("p")
     p.innerHTML = lvl
+    border.src = `https://raw.communitydragon.org/11.20/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/prestigeborders/theme-${Math.ceil(lvl/25)}-border.png`
+    border.className = "b"
     img.src = `https://raw.communitydragon.org/11.20/game/assets/ux/summonericons/profileicon${icon}.png`
     img.className = "a"
     img.style.backgroundColor = "rgba(128, 128, 128, 0.445)"
     img.style.backgroundImage = `linear-gradient(0deg, #65C178 ${percent}%, rgba(0, 0, 0, 0) ${percent}%)`
     legend.innerHTML = name
-    p.style.fontSize = "1.5vw"
-    p.style.fontWeight = 500
     fieldset.appendChild(legend)
     fieldset.appendChild(img)
+    fieldset.appendChild(border)
     fieldset.appendChild(p)
     fieldset.className = "player"
     fieldset.id = id
@@ -130,6 +122,7 @@ const createPlayer = (name, icon, id, lvl, percent) => {
 }
 
 let lobbyIds = []
+let riotIds = []
 
 connector.on('connect', (data) => {
     console.log(data)
@@ -137,76 +130,25 @@ connector.on('connect', (data) => {
 
     //lobby event
     setInterval(() => {
-        get("/lol-lobby/v2/comms/members").then(res => {
-            Object.keys(res.players).forEach(player => {
-                if (!lobbyIds.includes(res.players[player].summonerId)) {
-                    get(`/lol-summoner/v1/summoners/${res.players[player].summonerId}`).then(res2 => {
-                        lobbyIds.push(res.players[player].summonerId)
-                        createPlayer((res.players[player].role == "LEADER" ? (res.players[player].displayName + "👑") : res.players[player].displayName), res2.profileIconId, res.players[player].summonerId, res2.summonerLevel, res2.percentCompleteForNextLevel)
-                    })
+        
+        get("/lol-lobby/v2/lobby/members").then(res => {
+            lobbyIds.forEach(id => {
+                if (!riotIds.includes(id)) {
+                    lobbyIds.slice(lobbyIds.indexOf(id), 1)
+                    document.getElementById(id).remove()
                 }
             })
-            // lobbyIds.forEach(id => {
-            //     if (!Object.keys(res.players).includes(id)) {
-            //         lobbyIds.slice(lobbyIds.indexOf(id), 1)
-            //         document.getElementById(id).remove()
-            //     }
-            // })
+            riotIds.length = 0
+            Object.keys(res).forEach(async player => {
+                riotIds.push(res[player].summonerId)
+                if (lobbyIds.includes(res[player].summonerId)) return
+                lobbyIds.push(res[player].summonerId)
+                let percent = await get(`/lol-summoner/v1/summoners/${res[player].summonerId}`).then(res2 => res2.percentCompleteForNextLevel)
+                createPlayer(res[player].isLeader ? res[player].summonerName + "👑" : res[player].summonerName, res[player].summonerIconId, res[player].summonerId, res[player].summonerLevel, percent)
+            })
         })
-        
     }, 1000) 
 
-        // {
-        //   accountId: 2713467472864992,
-        //   displayName: 'yurms',
-        //   internalName: 'yurms',
-        //   nameChangeFlag: false,
-        //   percentCompleteForNextLevel: 20,
-        //   profileIconId: 1637,
-        //   puuid: '8740b9d7-8b79-584d-b8ea-83a4a3d407bd',
-        //   rerollPoints: {
-        //     currentPoints: 292,
-        //     maxRolls: 2,
-        //     numberOfRolls: 1,
-        //     pointsCostToRoll: 250,
-        //     pointsToReroll: 208
-        //   },
-        //   summonerId: 2713467472864992,
-        //   summonerLevel: 39,
-        //   unnamed: false,
-        //   xpSinceLastLevel: 634,
-        //   xpUntilNextLevel: 3072
-        // }
-        //  {  
-        //    address: '127.0.0.1'
-        //    port: 18633, 
-        //    username: 'riot',
-        //    password: H9y4kOYVkmjWu_5mVIg1qQ,
-        //    protocol: 'https'
-        //  }
-
-    //   {
-    //     gameName: 'RobJobs',
-    //     gameTag: 'Riven',
-    //     id: 'd05b035d-047c-5e8e-bf31-2f6eb27007c2@na1.pvp.net',
-    //     inviterId: '',
-    //     isMuted: false,
-    //     lastMessage: {
-    //       body: '.',
-    //       fromId: '8740b9d7-8b79-584d-b8ea-83a4a3d407bd@na1.pvp.net',
-    //       fromPid: '8740b9d7-8b79-584d-b8ea-83a4a3d407bd@na1.pvp.net',
-    //       fromSummonerId: 2713467472864992,
-    //       id: '1633753356968:89',
-    //       isHistorical: true,
-    //       timestamp: '2021-10-09T04:22:36.968Z',
-    //       type: 'chat'
-    //     },
-    //     name: 'FlyingPig014',
-    //     password: '',
-    //     pid: 'd05b035d-047c-5e8e-bf31-2f6eb27007c2@na1.pvp.net',
-    //     targetRegion: 'na1',
-    //     type: 'chat',
-    //     unreadMessageCount: 0
     document.getElementById("container").style.display = "flex"
     document.getElementById("waiting").style.display = "none"
     const node = document.getElementById("window")
@@ -234,10 +176,6 @@ connector.on('connect', (data) => {
         document.getElementById("cancel").style.display = "none"
         document.getElementById("cancel").style.opacity = 0
     })
-    //   },
-    // get("/lol-summoner/v1/current-summoner/autofill").then(res => {
-    //     console.log(res)
-    // })
 })
 
 // Start listening for the LCU client
